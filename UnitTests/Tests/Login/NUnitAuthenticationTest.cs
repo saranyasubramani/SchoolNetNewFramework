6a07aae67ca3a47ffbd464e83cc5cd5bc144f25c
saranya.subramani <saranya.subramani@Aspiresys.com>
2016-02-23 12:29:40 +0530

    committing the changes2

--- UnitTests/Tests/Login/NUnitAuthenticationTest.cs
+++ UnitTests/Tests/Login/NUnitAuthenticationTest.cs
@@ -76,22 +76,6 @@ namespace UnitTests.Tests.Login
             }
         }
 
-         [Test]
-        public void testClearForm1()
-        {
-            try
-            {
-                AuthenticationTest test = new AuthenticationTest();
-                test.testClearForm(this.SchoolNet());
-            }
-            catch (Exception e)
-            {
-                TestContext.WriteLine("\nInnerException:\n" + e.InnerException + "\nStackTrace:\n" + e.StackTrace, e);
-                new DriverCommands().GetScreenshotAndPageSource();
-                throw new Exception("\nInnerException:\n" + e.InnerException + "\nStackTrace:\n" + e.StackTrace, e);
-            }
-        }
-
         [Test]
         public void testInputAndSubmitForm()
         {
